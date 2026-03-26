fc320c02facc6625f5808c0cc194ac457bafe0ce
GenSpark Developer <developer@genspark.ai>
2026-03-26 05:18:14 +0000

    fix: String.raw 에서 script 닫는 태그 이슈 수정 - 버튼 클릭 작동 복구

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.html(HTML_CONTENT)
+  return c.html(HTML_CONTENT.replace('<\\/script>', '</script>'))
 })
 
 export default app
